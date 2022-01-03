26c6db4f9d8bfb2981ff060744a136cba166bb96
Flam4Ing <flam112@web.de>
2022-01-03 16:41:31 +0100

    Projektdateien hinzufügen.

---
+++ MyPlugIn.cs
@@ -0,0 +1,25 @@
+﻿using Aucotec.EngineeringBase.Client.Runtime;
+using System;
+using System.AddIn;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FAM_EB_WorksheetAttr
+{
+    /// <summary>
+    /// Implements Wizard FAM_EB_WS_Attr
+    /// </summary>
+    [AddIn("FAM_EB_WS_Attr", Description = "", Publisher = "TKE")]
+    public class MyPlugIn : PlugInWizard
+    {
+        /// <summary>
+        /// Runs the wizard.
+        /// </summary>
+        /// <param name="myApplication">Application object instance</param>	
+        public override void Run(Application myApplication)
+        {
+            // System.Diagnostics.Debug.WriteLine("Hello World!");
+        }
+    }
+}
